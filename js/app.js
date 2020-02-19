131642e115c29ec45963542f28314885b420cbf4
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2020-02-19 14:35:54 +0300

    ready day two

--- js/app.js
+++ js/app.js
@@ -1,10 +1,52 @@
+const DATA = {
+  whichSite: [
+    'landing',
+    'multiPage',
+    'onlineStore'
+  ],
+  price: [
+    4000,
+    8000,
+    26000
+  ],
+  desktopTemplates: [
+    50,
+    40,
+    30
+  ],
+  adapt: 20,
+  mobileTemplates: 15,
+  editable: 10,
+  metrikaYandex: [
+    500,
+    1000,
+    2000
+  ],
+  analyticsGoogle: [
+    850,
+    1350,
+    3000
+  ],
+  sendOrder: 500,
+  deadlineDay: [
+    [2,7],
+    [3, 10],
+    [7, 14],
+  ],
+  deadlinePercent: [
+    20,
+    17,
+    15
+  ]
+};
 const startButton = document.querySelector('.start-button'),
       form = document.querySelector('.main-form'),
       firstScreen = document.querySelector('.first-screen'),
       formCalculate = document.querySelector('.form-calculate'),
       endButton = document.querySelector('.end-button'),
       total = document.querySelector('.total'),
-      fastRange = document.querySelector('.fast-range');
+      fastRange = document.querySelector('.fast-range'),
+      totalPriceSum = document.querySelector('.total_price__sum');
 //console.dir(startButton);
 //console.log(startButton);
 
@@ -14,6 +56,56 @@ const showElem = (elem) => {
 };
 const hideElem = (elem) => {
   elem.style.display = 'none';
+};
+
+const priceCalculation = (elem) => {
+
+  let result = 0,
+      index = 0,
+      options = [];
+
+  if (elem.name === 'whichSite') {
+    for (const item of formCalculate.elements) {
+      if (item.type === 'checkbox') {
+        item.checked = false;
+      }
+    }
+    hideElem(fastRange);
+  }
+
+  for (const item of formCalculate.elements) {
+    if (item.name === 'whichSite' && item.checked) {
+      index = DATA.whichSite.indexOf(item.value);
+    } else if (item.classList.contains('calc-handler') && item.checked) {
+      options.push(item.value);
+    }
+  }
+
+  options.forEach((key) => {
+    // проверка на число
+    if (typeof(DATA[key]) === 'number') {
+      if (key === 'sendOrder') {
+        result += DATA[key]
+      } else {
+        result += DATA.price[index] * DATA[key] / 100
+      }
+    } else {
+      if (key === 'desktopTemplates') {
+        result += DATA.price[index] * DATA[key][index] / 100
+      } else {
+        result += DATA[key][index];
+      }
+    }
+
+
+
+  });
+
+  result += DATA.price[index];
+
+  totalPriceSum.textContent = result;
+
+
 };
 
 const handlerCallBackForm = (event) => {
@@ -29,6 +121,11 @@ const handlerCallBackForm = (event) => {
     // что будет происходить если елемент чекед тру, а что если false
     target.checked ? showElem(fastRange) : hideElem(fastRange);
   }
+
+  if (target.classList.contains('calc-handler')) {
+    priceCalculation(target);
+  }
+
 
 };
 
